197049d0e1bb57b5e5e90bc1b3a56ed992a7aa5c
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-12-11 15:29:01 +0600

    updated

--- app/page.js
+++ app/page.js
@@ -8,24 +8,13 @@ import LinkSection from "@/components/home/LinkSection";
 import ManageTeam from "@/components/home/manage/ManageTeam";
 import Form from "@/components/home/saving-form/Form";
 import Smartsite from "@/components/home/Smartsite";
-import Analytics from "@/components/home/smartSite/Analytics";
-import Bank from "@/components/home/smartSite/Bank";
-import Leads from "@/components/home/smartSite/Leads";
-import Messaging from "@/components/home/smartSite/Messaging";
-import SmartSite from "@/components/home/smartSite/SmartSite";
 import SwoppleSection from "@/components/home/SwoppleSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      <div className="grid grid-cols-1 auto-rows-min  md:grid-cols-2 2xl:grid-cols-3 gap-4 xl:gap-5 h-full items-stretch self-stretch lg:px-16 xl:px-48 mt-10 md:mt-6">
-        <Messaging />
-        <SmartSite />
-        <Analytics />
-        <Bank />
-        <Leads />
-      </div>
+      <Smartsite />
       <InteractiveSection />
       <ManageTeam />
       <Form />
